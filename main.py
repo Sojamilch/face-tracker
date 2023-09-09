da9103fcb38e88fef5799667285884f99e0cbaa0
Sojamilch <hlrockwelljones@gmail.com>
2023-09-09 23:28:22 +0100

    Added option to fullscreen output

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ argParser = argparse.ArgumentParser()
 
 # Show only bounding boxes
 argParser.add_argument("-b", "--blank", action='store_true')
-argParser.add_argument("-y", "--height", type=int,
-                       default=720, help="Camera viewport height in pixels")
-argParser.add_argument("-x", "--width", type=int, default=1280,
-                       help="Camera viewport width in pixels")
-argParser.add_argument("-fs", "--frameskips", type=int, default=4,
-                       help="Ammount of frames to skip, larger number reduces cpu usage")
-argParser.add_argument("-f", "--fps", type=int, default=30,
-                       help="fps limit")
+argParser.add_argument("-y", "--height", type=int, default=720, help="Camera viewport height in pixels")
+argParser.add_argument("-x", "--width", type=int, default=1280, help="Camera viewport width in pixels")
+argParser.add_argument("-fs", "--frameskips", type=int, default=4, help="Ammount of frames to skip, larger number reduces cpu usage")
+# argParser.add_argument("-f", "--fps", type=int, default=30, help="fps limit")
+argParser.add_argument("-f", "--fullscreen", type=bool, default=False, help="full screen")
 args = vars(argParser.parse_args())
+
 
 windowName = "Video"
 # Skipping frames reduces cpu-usage dramatically
@@ -27,15 +25,21 @@ blank = args["blank"]
 try:
     cascadePath = "venv\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml"
     faceCascade = cv2.CascadeClassifier(cascadePath)
-except:
+except BaseException:
     print("Invalid or missing cascade data path.")
     exit()
 
 
 def Render(frame, faces):
 
+    color = (int(0), int(255), int(0))
+
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, [x, y], [x+w, y+h], (0, 255, 0), 2)
+        cv2.rectangle(frame, [x, y], [x + w, y + h], tuple(color), 2)
+
+    if (args["fullscreen"]):
+        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     cv2.imshow(windowName, frame)
 
@@ -112,8 +116,8 @@ def main():
 
         count += 1
         if (count == 100):
-            time = (cv2.getTickCount() - time)/cv2.getTickFrequency()
-            fps = 100.0/time
+            time = (cv2.getTickCount() - time) / cv2.getTickFrequency()
+            fps = 100.0 / time
 
             print(f'FPS: {fps}', end='\r')
 
